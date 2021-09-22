cc35a9ba67231e5b0d856d4c81a2e433e9136730
Karina (Kae) Ramirez <klramirez665@gmail.com>
2021-09-21 23:23:30 -0700

    Add navigation between MainActivity and SearchActivity

--- app/src/main/java/com/example/photogalleryapp/SearchActivity.java
+++ app/src/main/java/com/example/photogalleryapp/SearchActivity.java
@@ -3,12 +3,22 @@ package com.example.photogalleryapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class SearchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search_view);
+        setContentView(R.layout.activity_search);
+    }
+
+    public void submitButton(View view) {
+        // TO DO
+    }
+
+    // Closes the search view and takes the user back to parent view (MainActivity)
+    public void cancelButton(View view) {
+        finish();
     }
 }
