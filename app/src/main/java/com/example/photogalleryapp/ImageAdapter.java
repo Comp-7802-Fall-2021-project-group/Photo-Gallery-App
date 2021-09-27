69b8a959db2ef87b41c878c5032e1d9bbeb4bd71
marc.ho <hsc8.dev@gmail.com>
2021-09-26 20:39:10 -0700

    add viewPager layout and implement left + right functionality

---
+++ app/src/main/java/com/example/photogalleryapp/ImageAdapter.java
@@ -0,0 +1,54 @@
+package com.example.photogalleryapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.viewpager.widget.PagerAdapter;
+
+public class ImageAdapter extends PagerAdapter {
+    Context context;
+
+    LayoutInflater mLayoutInflater;
+
+    ImageAdapter(Context context) {
+        this.context = context;
+        mLayoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return getImages.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == ((ConstraintLayout) object);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        View itemView = mLayoutInflater.inflate(R.layout.activity_main, container, false);
+
+        ImageView imageView = (ImageView) itemView.findViewById(R.id.imageView);
+        imageView.setImageResource(getImages[position]);
+        container.addView(itemView);
+
+        return itemView;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((ConstraintLayout) object);
+    }
+
+    private int[] getImages = new int[]{
+            R.drawable.vanone,
+            R.drawable.vantwo,
+            R.drawable.vanthree
+    };
+}
--- app/src/main/java/com/example/photogalleryapp/MainActivity.java
+++ app/src/main/java/com/example/photogalleryapp/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.photogalleryapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,11 +14,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ViewPager viewPager = findViewById(R.id.viewPager);
+        final ImageAdapter imageAdapter = new ImageAdapter(this);
+        viewPager.setAdapter(imageAdapter);
+        viewPager.setCurrentItem(imageAdapter.getCount() - 1);
+
+        imageControl();
     }
 
     // Navigate the user to the search view
     public void gotoSearch(View view) {
         Intent intent = new Intent(this, SearchActivity.class);
         startActivity(intent);
+    }
+
+
+    public void imageControl() {
+        final ViewPager viewPager = findViewById(R.id.viewPager);
+        final Button left = findViewById(R.id.left);
+        final Button right = findViewById(R.id.right);
+        left.setOnClickListener(v -> {
+            int tab = viewPager.getCurrentItem();
+            if (tab > 0) {
+                tab--;
+                viewPager.setCurrentItem(tab);
+            } else if (tab == 0) {
+                viewPager.setCurrentItem(tab);
+            }
+        });
+
+        right.setOnClickListener(v -> {
+            int tab = viewPager.getCurrentItem();
+            tab++;
+            viewPager.setCurrentItem(tab);
+        });
     }
 }
